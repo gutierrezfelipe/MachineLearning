a8a0b854a01bd47955d73b416d32af27e2f67fee
Felipe Gutierrez <gutierrezfeliped@gmail.com>
2022-04-04 20:22:46 -0300

    Assignment 1 - End of Class

--- Assignment/Assignment1/assignment1.py
+++ Assignment/Assignment1/assignment1.py
@@ -12,9 +12,61 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import   KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.feature_selection import SelectFromModel
+
+
+class SBS:
+    def __init__(self, estimator, k_features, scoring=accuracy_score,
+                 test_size=0.25, random_state=1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.test_size,
+                                                            random_state=self.random_state)
+
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train,
+                                 X_test, y_test, self.indices_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indices_, r=dim - 1):
+                score = self._calc_score(X_train, y_train,
+                                         X_test, y_test, p)
+                scores.append(score)
+                subsets.append(p)
+
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+
+        return self
+
+    def transform(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
 
 
 #Importing data
@@ -151,6 +203,22 @@ lr.fit(X_train, Y_train)
 print('Training accuracy:', lr.score(X_train, Y_train))
 print('Test accuracy:', lr.score(X_test, Y_test))
 
+'''
+# KNN
+knn = KNeighborsClassifier(n_neighbors=5)
 
+#selecting features
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
 
+#plotting performance of feature subsets
+k_feat = [len(k) for k in sbs.subsets_]
 
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+plt.show()
+'''
