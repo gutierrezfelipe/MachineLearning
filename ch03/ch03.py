faf53acb964c61d43e5e7a0498ed6059965468b3
gutierrezfelipe <gutierrezfeliped@gmail.com>
2022-04-03 21:35:45 -0300

    First corrections

--- ch03/ch03.py
+++ ch03/ch03.py
@@ -127,7 +127,7 @@ X_test_std = sc.transform(X_test)
 
 
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=1)
 ppn.fit(X_train_std, y_train)
 
 
@@ -159,7 +159,7 @@ print('Accuracy: %.2f' % ppn.score(X_test_std, y_test))
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
     # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
+    markers = ('o', 's', '^', 'v', '<')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
 
@@ -168,35 +168,36 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
+    # plot class examples
     for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], 
+        plt.scatter(x=X[y == cl, 0],
                     y=X[y == cl, 1],
-                    alpha=0.8, 
+                    alpha=0.8,
                     c=colors[idx],
-                    marker=markers[idx], 
-                    label=cl, 
+                    marker=markers[idx],
+                    label=f'Class {cl}',
                     edgecolor='black')
 
-    # highlight test samples
+    # highlight test examples
     if test_idx:
-        # plot all samples
+        # plot all examples
         X_test, y_test = X[test_idx, :], y[test_idx]
 
         plt.scatter(X_test[:, 0],
                     X_test[:, 1],
-                    c='',
+                    c='none',
                     edgecolor='black',
                     alpha=1.0,
                     linewidth=1,
                     marker='o',
-                    s=100, 
-                    label='test set')
+                    s=100,
+                    label='Test set')
 
 
 # Training a perceptron model using the standardized training data:
@@ -497,9 +498,9 @@ plt.show()
 
 
 
-ppn = SGDClassifier(loss='perceptron', n_iter=1000)
-lr = SGDClassifier(loss='log', n_iter=1000)
-svm = SGDClassifier(loss='hinge', n_iter=1000)
+ppn = SGDClassifier(loss='perceptron', max_iter=1000)
+lr = SGDClassifier(loss='log', max_iter=1000)
+svm = SGDClassifier(loss='hinge', max_iter=1000)
 
 
 # **Note**
