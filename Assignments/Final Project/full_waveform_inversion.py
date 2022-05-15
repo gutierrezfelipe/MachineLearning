6536f1f2a6eac10aebe6e31b7235574534da2e06
Felipe Gutierrez <gutierrezfeliped@gmail.com>
2022-05-14 21:12:48 -0300

    Assignment 3 - End of Class

---
+++ Assignments/Final Project/full_waveform_inversion.py
@@ -0,0 +1,10 @@
+# Felipe Derewlany Gutierrez
+# Full Waveform Iversion
+
+
+import numpy as np
+import time
+import math
+import pyqtgraph as pg
+import matplotlib.pyplot as plt
+from pyqtgraph.widgets.RawImageWidget import RawImageGLWidget
